6df7e2c434bc113ca27a7fa557c85708db987e0b
Igor Logvenyuk <igorlogvenyuk@Igors-MacBook-Pro.local>
2022-12-10 15:18:05 +0200

    created file with fetch methods for communication with the server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelServise';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +14,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
