42d96ed62c3b2f0baa1412b5776de1999b243a87
Igor Logvenyuk <igorlogvenyuk@Igors-MacBook-Pro.local>
2022-12-12 14:21:48 +0200

    now the random character component works

--- src/services/MarvelServise.js
+++ src/services/MarvelServise.js
@@ -14,12 +14,34 @@ class MarvelService {
         return await res.json();
     }
  
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        let descr = '';
+
+        if (char.description.length === 0) {
+            descr = 'Sorry, there is no character description =(';
+        } else if (char.description.length > 216) {
+            descr = `${char.description.slice(0, 213)} ...`;
+        } else {
+            descr = char.description;
+        }
+
+        return {
+            name: char.name,
+            description: descr,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
