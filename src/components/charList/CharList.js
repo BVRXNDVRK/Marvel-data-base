0235d9cc5efdd9f467bd437d47985f89a34f4b18
BVRXNDVRK <igxr.lxgvenyuk@gmail.com>
2023-03-27 12:23:30 +0300

    fixed focus bug in char list component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import Spinner from '../spinner/Spinner';
@@ -109,9 +109,13 @@ const CharList = (props) => {
         )
     }
 
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItems(charList), newitemsLoading)
+    }, [process]);
+
     return (
         <div className="char__list">
-            {setContent(process, () => renderItems(charList), newitemsLoading)} 
+            {elements} 
             <button 
             className="button button__main button__long"
             disabled={newitemsLoading}
